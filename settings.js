8fd9aa4f728620c6ad58aba1d073a2bf9c894d8a
weeeplaaay211-web <weeeplaaay211@gmail.com>
2025-08-09 20:21:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "B7R BOT-MD 🤖",
+  botOwner: 'B7R', // Your name
+  ownerNumber: '212621644603', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
